bd1dee7812480cf769b4fe51c3bbfc7081ff5306
apopo4 <apopo4@uic.edu>
2026-02-25 22:59:33 -0600

    added the login page needs to be updated for better UI.

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,7 +3,16 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/*  Login Screen */}
+     <Stack.Screen 
+        name="index" 
+        options={{ headerShown: false }} 
+      />
+      {/*  Main App (Tabs) */}
+      <Stack.Screen 
+        name="(tabs)" 
+        options={{ headerShown: false }} 
+      />
     </Stack>
   );
 }
