b34e620fe97e92d382ece0b6fc76ad6ccd6142c2
Joselyn De Loera <140840162+joselynloera@users.noreply.github.com>
2026-03-12 17:41:46 -0500

    completed events page!

--- frontend/app/(tabs)/events.tsx
+++ frontend/app/(tabs)/events.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from 'react-native';
+/*import { Text, View, StyleSheet } from 'react-native';
 
 export default function EventsScreen() {
   return (
@@ -17,5 +17,134 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+  },
+});
+*/
+
+// app/event-page.tsx
+import React from "react";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function EventPage() {
+  return (
+    <View style={styles.container}>
+
+      <ScrollView style={styles.scroll} contentContainerStyle={styles.eventList}>
+
+      {/* Header */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Upcoming Events</Text>
+      </View>
+
+        {/* Event Cards */}
+        <View style={styles.card}>
+          <Text style={styles.arrow}>→</Text>
+          <Text style={styles.title}>Event Name</Text>
+          <Text style={styles.info}>🗓️Event Date</Text>
+          <Text style={styles.info}>📍Location</Text>
+          <Text style={styles.desc}>description</Text>
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.arrow}>›</Text>
+          <Text style={styles.title}>Event Name</Text>
+          <Text style={styles.info}>📅Event Date</Text>
+          <Text style={styles.info}>📌Location</Text>
+          <Text style={styles.desc}>description</Text>
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.arrow}>{">"}</Text>
+          <Text style={styles.title}>Event Name</Text>
+          <Text style={styles.info}>📆Event Date</Text>
+          <Text style={styles.info}>🌍Location</Text>
+          <Text style={styles.desc}>description</Text>
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.title}>Event Name</Text>
+          <Text style={styles.info}>Event Date</Text>
+          <Text style={styles.info}>Location</Text>
+          <Text style={styles.desc}>description</Text>
+        </View>
+
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F2F2F2",
+  },
+
+  header: {
+    backgroundColor: "#082352",
+    paddingTop: 25,
+    paddingBottom: 25,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+
+  headerText: {
+    color: "#D40032",
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+
+  eventList: {
+    //padding: 20,
+    paddingBottom: 40,
+    //paddingTop: 0,
+  },
+
+  card: {
+    backgroundColor: "white",
+    borderRadius: 18,
+    padding: 18,
+    marginHorizontal: 20,
+    marginBottom: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 12,
+    elevation: 10,
+  },
+
+  arrow: {
+  position: "absolute",
+  top: 15,
+  right: 15,
+  fontSize: 24,
+  color: "#D40032"
+  },
+
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "navy",
+    marginBottom: 6,
+    //textAlign: "center", //maybe remove lets get opinions
+  },
+
+  info: {
+    fontSize: 14,
+    color: "#636363",
+    marginBottom: 3,
+    textAlign: "center",
+  },
+
+  desc: {
+    marginTop: 8,
+    fontSize: 14,
+    color: "#444",
+    textAlign: "center",
+  },
+
+  scroll: {
+  flex: 1,
   },
 });
